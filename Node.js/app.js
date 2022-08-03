4f176fc7d54cd6a511593eee0a1683d62f33faab
NicksonEdbert <nicksonedbert@gmail.com>
2022-08-03 10:48:49 -0700

    added node.js file

---
+++ Node.js/app.js
@@ -0,0 +1,267 @@
+/*
+    SETUP
+*/
+var express = require("express"); // We are using the express library for the web server
+var app = express(); // We need to instantiate an express object to interact with the server in our code
+
+// Configuring Express to Handle JSON and Form Data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/*
+http://flip1.engr.oregonstate.edu:9234/
+*/
+PORT = 9234; // Set a port number at the top so it's easy to change in the future
+
+// Database
+var db = require("./database/db-connector");
+
+// Handlebars
+var Promise = require("bluebird");
+
+const { engine } = require("express-handlebars");
+var exphbs = require("express-handlebars"); // Import express-handlebars
+app.engine(".hbs", engine({ extname: ".hbs" })); // Create an instance of the handlebars engine to process templates
+app.set("view engine", ".hbs"); // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+// Serving static files in Express
+app.use(express.static("public"));
+
+/*
+    ROUTES
+*/
+
+// app.get("/", function (req, res) {
+//    let query1 = "SELECT * FROM Customers;"; // Define our query
+//    let query2 = "SELECT * FROM Orders;"; // Define our query
+//    let query3 = "SELECT * FROM Order_Details;"; // Define our query
+//    let query4 = "SELECT * FROM Products;"; // Define our query
+
+//    db.pool.query(query1, function (error, rows, fields) {
+//       // Execute the query
+
+//       let customers = rows;
+
+//       db.pool.query(query2, function(error, rows, fields){
+//          let ordersTable = rows;
+
+//          db.pool.query(query3, function(error, rows, fields){
+//             let ordersDetailsTable = rows;
+
+//             db.pool.query(query4, function(error, rows, fields){
+//                let productsTable = rows;
+
+//                return res.render('index', {data: customers, orders: ordersTable, details: ordersDetailsTable, products: productsTable});
+//             })
+
+//          })
+
+//       })
+//    }); // an object where 'data' is equal to the 'rows' we
+// }); // received back from the query
+
+app.get("/", function (req, res) {
+   let query1 = "SELECT * FROM Customers;"; // Define our query
+   let query2 = "SELECT * FROM Orders;"; // Define our query
+   let query3 = "SELECT * FROM Order_Details;"; // Define our query
+   let query4 = "SELECT * FROM Products;"; // Define our query
+
+   switch (req.query.select) {
+      case "Customers":
+         db.pool.query(query1, function (error, rows, fields) {
+            // Execute the query
+      
+            let datas = rows;
+      
+            return res.render('index', {data: datas, customers: datas});
+      
+         });
+         break;
+      case "Orders":
+         db.pool.query(query2, function (error, rows, fields) {
+            // Execute the query
+      
+            let datas = rows;
+      
+            return res.render('index', {data: datas, orders: datas});
+      
+         });
+         break;
+      case "Order_Details":
+         db.pool.query(query3, function (error, rows, fields) {
+            // Execute the query
+      
+            let datas = rows;
+      
+            return res.render('index', {data: datas, details: datas});
+      
+         });
+         break;
+      case "Products":
+         db.pool.query(query4, function (error, rows, fields) {
+            // Execute the query
+      
+            let datas = rows;
+      
+            return res.render('index', {data: datas, products: datas});
+      
+         });
+         break;
+
+      case "Select a Table":
+         db.pool.query(query1, function (error, rows, fields) {
+            // Execute the query
+      
+            let datas = rows;
+      
+            return res.render('index', {data: datas, customers: datas});
+      
+         });
+         break;
+   }
+
+}); // received back from the query
+
+// POST ROUTES
+app.post("/add-person-ajax", function (req, res) {
+   // Capture the incoming data and parse it back to a JS object
+   let data = req.body;
+
+   // console.log(data)
+
+   // Capture NULL values
+   let phone = parseInt(data.phone);
+   if (isNaN(phone)) {
+      phone = "NULL";
+   }
+
+   // Create the query and run it on the database
+   query1 = `INSERT INTO Customers (first_name, last_name, phone) VALUES ('${data.first_name}', '${data.last_name}', ${phone});`;
+   db.pool.query(query1, function (error, rows, fields) {
+      // Check to see if there was an error
+      if (error) {
+         // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+         console.log(error);
+         res.sendStatus(400);
+      } else {
+         // If there was no error, perform a SELECT * on bsg_people
+         query2 = `SELECT * FROM Customers;`;
+         db.pool.query(query2, function (error, rows, fields) {
+            // If there was an error on the second query, send a 400
+            if (error) {
+               // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+               console.log(error);
+               res.sendStatus(400);
+            }
+            // If all went well, send the results of the query back.
+            else {
+               res.send(rows);
+            }
+         });
+      }
+   });
+});
+
+// app.post("/add-person-form", function (req, res) {
+//    // Capture the incoming data and parse it back to a JS object
+//    let data = req.body;
+
+//    // Capture NULL values
+//    let phone = parseInt(data.phone);
+//    if (isNaN(phone)) {
+//       phone = "NULL";
+//    }
+
+//    // Create the query and run it on the database
+//    query1 = `INSERT INTO Customers (first_name, last_name, phone) VALUES ('${data["input-fname"]}', '${data["input-lname"]}', '${data["input-phone"]}')`;
+//    db.pool.query(query1, function (error, rows, fields) {
+//       // Check to see if there was an error
+//       if (error) {
+//          // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+//          console.log(error);
+//          res.sendStatus(400);
+//       }
+//       // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+//       // presents it on the screen
+//       else {
+//          res.redirect("/");
+//       }
+//    });
+// });
+
+// Delete Route
+
+app.delete("/delete-person-ajax/", function (req, res, next) {
+   let data = req.body;
+   let personID = parseInt(data.customer_id);
+   let deleteBsg_Cert_People = `DELETE FROM Customers WHERE customer_id = ?`;
+
+   // Run the 1st query
+   db.pool.query(
+      deleteBsg_Cert_People,
+      [personID],
+      function (error, rows, fields) {
+         if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+         } else {
+            res.sendStatus(204);
+         }
+      }
+   );
+});
+
+app.put("/put-person-ajax", function (req, res, next) {
+   let data = req.body;
+
+   let homeworld = parseInt(data.homeworld);
+   let person = parseInt(data.fullname);
+
+   let queryUpdateWorld = `UPDATE Customers SET phone = ? WHERE customer_id = ?`;
+   let selectWorld = `SELECT * FROM Customers WHERE customer_id = ?`;
+
+   // Run the 1st query
+   db.pool.query(
+      queryUpdateWorld,
+      [homeworld, person],
+      function (error, rows, fields) {
+         if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+         }
+
+         // If there was no error, we run our second query and return that data so we can use it to update the people's
+         // table on the front-end
+         else {
+            // Run the second query
+            db.pool.query(
+               selectWorld,
+               [person],
+               function (error, rows, fields) {
+                  if (error) {
+                     console.log(error);
+                     res.sendStatus(400);
+                  } else {
+                     res.send(rows);
+                  }
+               }
+            );
+         }
+      }
+   );
+});
+
+/*
+    LISTENER
+*/
+
+app.listen(PORT, function () {
+   // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+   console.log(
+      "Express started on http://localhost:" +
+         PORT +
+         "; press Ctrl-C to terminate."
+   );
+});
---
+++ Node.js/public/js/add_person.js
@@ -0,0 +1,100 @@
+// Get the objects we need to modify
+let addPersonForm = document.getElementById("add-person-form-ajax");
+
+// Modify the objects we need
+addPersonForm.addEventListener("submit", function (e) {
+   // Prevent the form from submitting
+   e.preventDefault();
+
+   // GET DATA
+   // Get form fields we need to get data from
+   let inputFirstName = document.getElementById("input-fname");
+   let inputLastName = document.getElementById("input-lname");
+   let inputPhone = document.getElementById("input-phone");
+
+   // Get the values from the form fields
+   let firstNameValue = inputFirstName.value;
+   let lastNameValue = inputLastName.value;
+   let phoneValue = inputPhone.value;
+
+   // PUT DATA
+   // Put our data we want to send in a javascript object
+   let data = {
+      first_name: firstNameValue,
+      last_name: lastNameValue,
+      phone: phoneValue,
+   };
+
+   // Setup our AJAX request
+   var xhttp = new XMLHttpRequest();
+   xhttp.open("POST", "/add-person-ajax", true);
+   xhttp.setRequestHeader("Content-type", "application/json");
+
+   // Tell our AJAX request how to resolve
+   xhttp.onreadystatechange = () => {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+         // Add the new data to the table
+         addRowToTable(xhttp.response);
+
+         // Clear the input fields for another transaction
+         inputFirstName.value = "";
+         inputLastName.value = "";
+         inputPhone.value = "";
+      } else if (xhttp.readyState == 4 && xhttp.status != 200) {
+         console.log("There was an error with the input.");
+      }
+   };
+
+   // Send the request and wait for the response
+   xhttp.send(JSON.stringify(data));
+});
+
+// Creates a single row from an Object representing a single record from
+// bsg_people
+addRowToTable = (data) => {
+   // Get a reference to the current table on the page and clear it out.
+   let currentTable = document.getElementById("people-table");
+
+   // Get the location where we should insert the new row (end of table)
+   let newRowIndex = currentTable.rows.length;
+
+   // Get a reference to the new row from the database query (last object)
+   let parsedData = JSON.parse(data);
+   let newRow = parsedData[parsedData.length - 1];
+
+   // Create a row and 4 cells
+   let row = document.createElement("TR");
+   let idCell = document.createElement("TD");
+   let firstNameCell = document.createElement("TD");
+   let lastNameCell = document.createElement("TD");
+   let phoneCell = document.createElement("TD");
+
+   // Fill the cells with correct data
+   idCell.innerText = newRow.id;
+   firstNameCell.innerText = newRow.first_name;
+   lastNameCell.innerText = newRow.last_name;
+   phoneCell.innerText = newRow.phone;
+
+   // Add the cells to the row
+   row.appendChild(idCell);
+   row.appendChild(firstNameCell);
+   row.appendChild(lastNameCell);
+   row.appendChild(phoneCell);
+
+   // Add a row attribute so the deleteRow function can find a newly added row
+   row.setAttribute("data-value", newRow.id);
+
+   // Add the row to the table
+   currentTable.appendChild(row);
+
+   // Start of new Step 8 code for adding new data to the dropdown menu for updating people
+
+   // Find drop down menu, create a new option, fill data in the option (full name, id),
+   // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
+   let selectMenu = document.getElementById("mySelect");
+   let option = document.createElement("option");
+   option.text = newRow.first_name + " " + newRow.last_name;
+   option.value = newRow.customer_id;
+   selectMenu.add(option);
+   // End of new step 8 code.
+};
